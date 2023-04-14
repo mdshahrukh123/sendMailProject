8a3b3be0f8f5905de4330d9b75a023620036715c
shahrukh <mohdshahrukhmzn1@gmail.com>
2023-04-14 12:51:49 +0530

    first commit

--- SendMailJava/src/main/java/org/geekster/MailConstants.java
+++ SendMailJava/src/main/java/org/geekster/MailConstants.java
@@ -2,10 +2,10 @@ package org.geekster;
 
 public class MailConstants {
 
-    public static final String SENDER = "mainakgh1@gmail.com";
+    public static final String SENDER = "mohdshahrukhmzn1@gmail.com";
 
     // receiver email
-    public static final String RECEIVERMAILADDRESS = "dpbehera1997@gmail.com";
+    public static final String RECEIVERMAILADDRESS = "mdshahrukhmzn2000@gmail.com";
 
     //subject of the mail
     public static final String SUBJECT = "Testing Java mail : 12-April-2023";
